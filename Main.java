79f8df4f6bace87a2a8b47dc107d314340ef717e
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-07-13 20:00:35 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
@@ -22,4 +30,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
+     public static void main(String[] args) {
+        launch(args);
+    }
+}
